56a8a4283c2b7cce295177861ff868773cf65c03
dreevo <sidedark85@gmail.com>
2022-04-21 18:17:25 +0100

    failing build

--- src/test/java/com/dreevo/jenkinsdemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/dreevo/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.dreevo.jenkinsdemo;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class JenkinsDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(true).isEqualTo(false);
 	}
 
 }
